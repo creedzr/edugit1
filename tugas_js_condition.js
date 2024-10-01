f01eb4bfc52bfee787d61a66985fbe5ed535da67
agum.ruswandi10@gmail.com <agum.ruswandi10@gmail.com>
2024-10-01 13:37:02 +0700

    menambahkan tugas_js_condition.js

---
+++ tugas_js_condition.js
@@ -0,0 +1,8 @@
+// Deklarasi variabel x dan y
+let x = 10; // Misalkan x = 10
+let y = 5;  // Misalkan y = 5
+
+// Membuat kondisi dengan if
+if (x > y) {
+    console.log("Horee saya bisa!"); // Pesan yang muncul jika x lebih besar dari y
+}
