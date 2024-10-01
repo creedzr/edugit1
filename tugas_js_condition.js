942a7968ce05b2ef6d4305da09531fa5ee29b2b5
creed <Agum.ruswandi10@gmail.com>
2024-10-01 13:38:18 +0700

    Update tugas_js_condition.js

--- tugas_js_condition.js
+++ tugas_js_condition.js
@@ -1,8 +1,6 @@
-// Deklarasi variabel x dan y
-let x = 10; // Misalkan x = 10
-let y = 5;  // Misalkan y = 5
 
-// Membuat kondisi dengan if
+let x = 10; 
+let y = 5;  
+
 if (x > y) {
-    console.log("Horee saya bisa!"); // Pesan yang muncul jika x lebih besar dari y
-}
+    console.log("Horee saya bisa!");
